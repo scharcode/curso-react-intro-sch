4fa226f9d37e704f1f8d274bff5d13933eaf6015
Andres Soret <andressocha230@gmail.com>
2024-01-03 19:02:47 -0500

    Clase #6 - Eventos en React: onClick, onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -5,6 +5,12 @@ function TodoSearch() {
     <input 
       placeholder="Buscar TODO"
       className="TodoSearch"
+      onChange={(event) => {
+        console.log('Escribiste en el TodoSearch')
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.value)
+      }}
     />
   );
 }
